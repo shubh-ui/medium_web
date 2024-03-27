b1f8a514f15eeafecbdf542ca1650f9057b66d07
Shubham <shubhpatil8510@gmail.com>
2024-03-27 12:47:20 +0530

    initial setup app routes

---
+++ blogging website - frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import Navabar from "./components/navbar.component";
+import { Routes, Route } from "react-router-dom"
+import UserAuthForm from "./pages/userAuthForm.page";
+
+const App = () => {
+    return (
+      <>
+        <Routes>
+          <Route path="/" element={<Navabar />}>
+            <Route path="/signin" element={<UserAuthForm type="sign-in" />} />
+            <Route path="/signup" element={<UserAuthForm type="sign-up" />} />
+          </Route>
+        </Routes>
+      </>
+    );
+}
+
+export default App;
