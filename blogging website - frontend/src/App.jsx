a1b52814bb143090d9a436979d4ac3da6f1a3471
Shubham <shubhpatil8510@gmail.com>
2024-04-14 22:06:31 +0530

    userContext created and userauth added into that for global state

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,16 +1,35 @@
 import Navabar from "./components/navbar.component";
 import { Routes, Route } from "react-router-dom"
 import UserAuthForm from "./pages/userAuthForm.page";
+import { createContext, useEffect, useState } from "react";
+import { lookInSession } from "./common/session";
+
+export const userContext = createContext({});
+
 
 const App = () => {
+
+  const [userAuth, setUserAuth] = useState({});
+
+  useEffect(() => {
+    let userInSession = lookInSession("user");
+
+    userInSession
+      ? setUserAuth(JSON.parse(userInSession))
+      : setUserAuth({ access_token: null });
+  },[]);
+
+
     return (
       <>
-        <Routes>
+       <userContext.Provider value={{userAuth, setUserAuth}}>
+       <Routes>
           <Route path="/" element={<Navabar />}>
             <Route path="/signin" element={<UserAuthForm type="sign-in" />} />
             <Route path="/signup" element={<UserAuthForm type="sign-up" />} />
           </Route>
         </Routes>
+       </userContext.Provider>
       </>
     );
 }
