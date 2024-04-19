8881019fe57b5d0b970b79ef3085386a92f62442
Shubham <shubhpatil8510@gmail.com>
2024-04-20 01:59:23 +0530

    added navabar for blog editor page

--- blogging website - frontend/src/components/blog-editor.component.jsx
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -1,6 +1,31 @@
+import { Link } from "react-router-dom"
+import Logo  from "../imgs/logo.png"
+import AnimationWrapper from "../common/page-animation";
+import BlogBanner from "../imgs/blog banner.png"
+
 const BlogEditor = () => {
     return (
-        <h1>This is commin gfrom blog editor component!</h1>
+        <>
+            <nav className="navbar">
+                <Link to="/" className="flex w-10">
+                    <img src={Logo} alt="Homelogo" />
+                </Link>
+
+                <p className="max-md:hidden text-black w-full line-clamp-1">New Blog</p>
+
+                <div className="flex gap-4 ml-auto">
+                    <button className="btn-dark py-2">
+                        Publish
+                    </button>
+
+                    <button className="btn-light py-2">
+                        Save Draft
+                    </button>
+                </div>
+            </nav>  
+
+               
+        </>
     )
 }
 
