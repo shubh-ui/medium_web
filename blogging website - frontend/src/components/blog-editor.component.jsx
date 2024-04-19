835b7f90deab484732a0c7a37cdf2bd04b9d80a7
Shubham <shubhpatil8510@gmail.com>
2024-04-20 00:34:02 +0530

    Blog editor component added

---
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -0,0 +1,7 @@
+const BlogEditor = () => {
+    return (
+        <h1>This is commin gfrom blog editor component!</h1>
+    )
+}
+
+export default BlogEditor;
--- blogging website - frontend/src/pages/editor.pages.jsx
+++ blogging website - frontend/src/pages/editor.pages.jsx
@@ -1,13 +1,16 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { userContext } from "../App";
 import { Navigate } from "react-router-dom";
+import BlogEditor from "../components/blog-editor.component";
 
 const Editor = () => {
+
+    const [editorState, setEditorState] = useState("editor")
 
     let { userAuth: { access_token} } = useContext(userContext);
 
     return (
-        access_token === null ? <Navigate to="/signin" /> : <h1>You can access the editor page</h1>
+        access_token === null ? <Navigate to="/signin" /> : editorState === "editor" ? <BlogEditor /> : <h1>Publish form</h1>
     )
 }
 
