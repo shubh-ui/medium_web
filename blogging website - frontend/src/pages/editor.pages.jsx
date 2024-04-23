0054c20483f0f1a2fbec65621aab81c37edda90c
Shubham <shubhpatil8510@gmail.com>
2024-04-24 00:50:14 +0530

    global state created for blogeditor

--- blogging website - frontend/src/pages/editor.pages.jsx
+++ blogging website - frontend/src/pages/editor.pages.jsx
@@ -2,25 +2,22 @@ import { useContext, useState } from "react";
 import { userContext } from "../App";
 import { Navigate } from "react-router-dom";
 import BlogEditor from "../components/blog-editor.component";
+// import { createContext } from "react";
 
-const Editor = () => {
+const blogStructure = {
+    title:'',
+    banner:'',
+    content:[],
+    tags:[],
+    desc:'',
+    auther:{ personal_info:{ } }
+}
 
-    const blogStructure = {
-        title:'',
-        banner:'',
-        content:[],
-        tags:[],
-        desc:'',
-        auther:{ personal_info:{ } }
-    }
-
-    const [editorState, setEditorState] = useState("editor")
 
     let { userAuth: { access_token} } = useContext(userContext);
 
     return (
         access_token === null ? <Navigate to="/signin" /> : editorState === "editor" ? <BlogEditor /> : <h1>Publish form</h1>
     )
-}
 
 export default Editor;
