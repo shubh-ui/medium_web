687e4849f453023257f72541ba36d521e8b7d9a2
Shubham <shubhpatil8510@gmail.com>
2024-04-24 00:51:09 +0530

    Context API created and passed global state

--- blogging website - frontend/src/pages/editor.pages.jsx
+++ blogging website - frontend/src/pages/editor.pages.jsx
@@ -2,7 +2,7 @@ import { useContext, useState } from "react";
 import { userContext } from "../App";
 import { Navigate } from "react-router-dom";
 import BlogEditor from "../components/blog-editor.component";
-// import { createContext } from "react";
+import { createContext } from "react";
 
 const blogStructure = {
     title:'',
@@ -13,11 +13,22 @@ const blogStructure = {
     auther:{ personal_info:{ } }
 }
 
+export const editorContext = createContext({ });
+
+const Editor = () => {
+
+    const [editorState, setEditorState] = useState("editor");
+    const [blog, setBlog] = useState(blogStructure);
 
     let { userAuth: { access_token} } = useContext(userContext);
 
     return (
-        access_token === null ? <Navigate to="/signin" /> : editorState === "editor" ? <BlogEditor /> : <h1>Publish form</h1>
+        <editorContext.Provider value={{ blog, setBlog, editorState, setEditorState }}>
+            {
+                access_token === null ? <BlogEditor /> : editorState === "editor" ? <BlogEditor /> : <h1>Publish form</h1>
+            }
+        </editorContext.Provider>
     )
+}
 
 export default Editor;
