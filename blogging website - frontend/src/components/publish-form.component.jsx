085857bbf53487675fdd59c548e239fa2223dd3f
Shubham <shubhpatil8510@gmail.com>
2024-04-26 00:31:27 +0530

    intial setup done for publish form

--- blogging website - frontend/src/components/publish-form.component.jsx
+++ blogging website - frontend/src/components/publish-form.component.jsx
@@ -1,6 +1,28 @@
+import { Toaster } from "react-hot-toast";
+import AnimationWrapper from "../common/page-animation";
+import { useContext } from "react";
+import { editorContext } from "../pages/editor.pages";
+
 const PublishForm = () => {
+
+    let { setEditorState } = useContext(editorContext);
+
+
+    const handleCloseEvent = () => {
+        setEditorState("editor");
+    }
     return (
-        <h1>This is Publish Form</h1>
+        <AnimationWrapper>
+            <section>
+                <Toaster />
+
+                <button className="w-12 h-12 right-[5vw] absolute z-10 top-[5%] lg:top-[10%]"
+                    onClick={handleCloseEvent}
+                >
+                <i className="fi fi-br-cross"></i>
+                </button>
+            </section>
+        </AnimationWrapper>
     )
 }
 
