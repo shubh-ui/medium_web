e478470378970d3fb737f4b2b6da11ddecdd3a01
Shubham <shubhpatil8510@gmail.com>
2024-04-28 00:42:35 +0530

    accessing the editor context values, UI change of preview inputes and changing the title from preview added

--- blogging website - frontend/src/components/publish-form.component.jsx
+++ blogging website - frontend/src/components/publish-form.component.jsx
@@ -5,17 +5,23 @@ import { editorContext } from "../pages/editor.pages";
 
 const PublishForm = () => {
 
-    let { setEditorState } = useContext(editorContext);
-    let { blog: {banner, title, desc} } = useContext(editorContext);
+    let { blog, blog:{ title, banner, tags, desc, content}, setBlog, textEditor, setTextEditor, editorState, setEditorState } = useContext(editorContext);
+    let {  } = useContext(editorContext);
 
 
     const handleCloseEvent = () => {
         setEditorState("editor");
     }
+
+    const handleBlogTitleChange = (e) => {
+        let input = e.target;
+        setBlog({ ...blog , title: input.value})
+    }
+    
     return (
       <AnimationWrapper>
-        <section>
-          <Toaster />
+        <section className="w-screen min-h-screen grid items-center lg:grid-cols-2 py-16 lg:gap-4" >
+          <Toaster /> 
 
           <button
             className="w-12 h-12 right-[5vw] absolute z-10 top-[5%] lg:top-[10%]"
@@ -30,7 +36,15 @@ const PublishForm = () => {
               <img src={banner} alt="" />
             </div>
             <h1 className="text-4xl font-medium-2 mt-2 leading-tight line-clamp-2">{title}</h1>
-            <p className="font-gelasio line-clamp-2">{desc}</p>
+            <p className="font-gelasio line-clamp-2 text-xl leading-7 mt-4">{desc}</p>
+          </div>
+
+          <div className="border-grey lg:border-1 lg:pl-8">
+            <h1 className="to-dark-grey mb-2 mt-9">Blog Title</h1>
+            <input className="input-box pl-4" type="text" placeholder="Blog Title" defaultValue={title} onChange={handleBlogTitleChange} />
+
+            <h1 className="to-dark-grey mb-2 mt-9">Short Description about your blog.</h1>
+
           </div>
         </section>
       </AnimationWrapper>
