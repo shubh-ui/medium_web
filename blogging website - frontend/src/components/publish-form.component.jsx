2e9e55eb6cf2fd9753c6903f04aedbe49e9f8f46
Shubham <shubhpatil8510@gmail.com>
2024-04-28 15:10:01 +0530

    tag added and tag handle function created

--- blogging website - frontend/src/components/publish-form.component.jsx
+++ blogging website - frontend/src/components/publish-form.component.jsx
@@ -2,12 +2,14 @@ import { Toaster } from "react-hot-toast";
 import AnimationWrapper from "../common/page-animation";
 import { useContext } from "react";
 import { editorContext } from "../pages/editor.pages";
+import Tags from "./tags.component";
 
 const PublishForm = () => {
 
     let { blog, blog:{ title, banner, tags, desc, content}, setBlog, textEditor, setTextEditor, editorState, setEditorState } = useContext(editorContext);
     let {  } = useContext(editorContext);
     const characterLimit =200;
+    const tagLimit = 10;
 
 
     const handleCloseEvent = () => {
@@ -22,6 +24,25 @@ const PublishForm = () => {
     const handleBlogDescriptionChange = (e) => {
         let input = e.target;
         setBlog({ ...blog, desc: input.value})
+    }
+
+    const handleTitileKeyDown = (e) => {
+        if(e.keyCode == 13) { //Enter Key
+            e.preventDefault();
+        }
+    }
+
+    const handleKeyDown = (e) => {
+      if(e.keyCode == 13 || e.keyCode == 188) {
+        
+        let tag = e.target.value;
+        if(tags?.length < tagLimit) {
+          if(!tags.includes(tag) && tag?.length) {
+            setBlog({ ...blog, tags: [...tags, tag]})
+          }
+          e.target.value ="";
+        }
+      }
     }
     
     return (
@@ -68,7 +89,31 @@ const PublishForm = () => {
               defaultValue={desc}
               maxLength={characterLimit}
               onChange={handleBlogDescriptionChange}
+              onKeyDown={handleTitileKeyDown}
             ></textarea>
+            <p className="mt-1 text-dark-grey text-sm text-right">
+              {characterLimit - desc.length} Characters left
+            </p>
+
+            <h1 className="mt-1 text-dark-grey text-sm">
+              Tags - (Helps to search and rank the blog){" "}
+            </h1>
+
+            <div className="relative input-box pl-2 pr-2 pb-4">
+              <input
+                type="text"
+                placeholder="Topic"
+                className="sticky input-box bg-white top-0 left-0 pl-4 pb-3 focus:bg-white"
+                onKeyDown={handleKeyDown}
+              />
+
+              {
+                tags.map((tag, i)=> (
+                  <Tags tag={tag} key={i} />
+                ))
+              }
+
+            </div>
           </div>
         </section>
       </AnimationWrapper>
