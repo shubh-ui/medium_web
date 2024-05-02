1ba48e58cc373982a4d808521332b2482083bf86
Shubham <shubhpatil8510@gmail.com>
2024-05-02 20:13:53 +0530

    img upload by file added into tools of editor

--- blogging website - frontend/src/components/tools.component.jsx
+++ blogging website - frontend/src/components/tools.component.jsx
@@ -32,6 +32,59 @@ const uploadImageByUrl = (url) => {
     });
 };
 
+const uploadImageByFile = (file) => {
+    return uploadImage(file).then(url => {
+        if (url) {
+            return {
+                success: 1,
+                file: { url }
+            };
+        }
+    });
+};
+
+const uploadImage = async (file) => {
+    // Implement your image upload logic here
+    // This could involve using a library or making an API call to upload the image file
+    // For now, let's assume it simply returns a mock URL after some processing
+    console.log(file);
+    let imageUrl;
+
+    if(file) {
+        // const toastId = toast.loading('Loading...');
+        const context = "/api";
+        const urlCd = "/upload";
+        const formData = new FormData();
+        formData.append('image', file);
+    
+        try {
+          const response = await axios.post(context + urlCd, formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          });
+          console.log(response)
+        //   toast.dismiss(toastId);
+          if(response.data.imageUrl){
+
+            // setBlog({ ...blog, banner:response.data.imageUrl})
+            imageUrl = response.data.imageUrl;
+            // blogBannerRef.current.src = response.data.imageUrl;
+            return imageUrl;
+
+          }
+        } catch (error) {
+          console.error('Error :', error);
+        }
+    // return new Promise((resolve, reject) => {
+    //     // Simulating image upload process
+    //     setTimeout(() => {
+    //         // Replace this with actual URL
+    //         resolve(imageUrl);
+    //     }, 2000); // Simulating 2 seconds of upload time
+    // });
+}  
+};
 
 
 export const tools = {
