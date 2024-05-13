0d8443da6445efd64f6144e8fb858ebf46a056ec
shubham <shubhpatil8510@gmail.com>
2024-05-13 23:48:23 +0530

    UI Changes and current route state added

--- blogging website - frontend/src/components/inpage-navigation.component.jsx
+++ blogging website - frontend/src/components/inpage-navigation.component.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const [ inPageNaveIndex, setInPageNaveIndex] = useState(0);
 
 const InpageNavigation = ({ Routes }) => {
   return (
-    <div>
-        {
-            Routes.map(route => (
-                <button>
-                    { route }
-                </button>
-            ))
-        }
-    </div>
-  )
+    <>
+      <div className='relative mb-8 border-b border-grey flex flex-nowrap overflow-x-auto'>
+        {Routes.map((route, i) => (
+          <button key={i} className={'p-4 px-5 capitalize ' (inPageNaveIndex == i ? "text-black" : "text-grey")}>{route}</button>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default InpageNavigation
