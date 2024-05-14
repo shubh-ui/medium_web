0905e1c8fa2fdf57f7b32d4fa3e7f8b146784f02
shubham <shubhpatil8510@gmail.com>
2024-05-14 23:12:53 +0530

    fixed some issue in inpagenavigation

--- blogging website - frontend/src/components/inpage-navigation.component.jsx
+++ blogging website - frontend/src/components/inpage-navigation.component.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react'
 
-const [ inPageNaveIndex, setInPageNaveIndex] = useState(0);
-
 const InpageNavigation = ({ Routes }) => {
+  const [ inPageNaveIndex, setInPageNaveIndex] = useState(0);
+
   return (
     <>
-      <div className='relative mb-8 border-b border-grey flex flex-nowrap overflow-x-auto'>
-        {Routes.map((route, i) => (
-          <button key={i} className={'p-4 px-5 capitalize ' (inPageNaveIndex == i ? "text-black" : "text-grey")}>{route}</button>
-        ))}
+      <div className='relative mb-8 border-b bg-white border-grey flex flex-nowrap overflow-x-auto'>
+        {Routes.map((route, i) => 
+          {
+            return <button key={i} className= {'p-4 px-5 capitalize '+ (inPageNaveIndex == i ? "text-black" : "text-grey")}>{route}</button>
+          }
+        )}
       </div>
     </>
   );
