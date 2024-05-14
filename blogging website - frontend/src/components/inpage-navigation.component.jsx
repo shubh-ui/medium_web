b3ffacf04250b79154062d2306f2d826729a798d
shubham <shubhpatil8510@gmail.com>
2024-05-14 23:55:09 +0530

    InpageNavigation active route UI changes done

--- blogging website - frontend/src/components/inpage-navigation.component.jsx
+++ blogging website - frontend/src/components/inpage-navigation.component.jsx
@@ -1,16 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-const InpageNavigation = ({ Routes }) => {
-  const [ inPageNaveIndex, setInPageNaveIndex] = useState(0);
+const InpageNavigation = ({ Routes, defaultActiveIndex = 0 }) => {
+  const [ inPageNaveIndex, setInPageNaveIndex] = useState(defaultActiveIndex);
+  const activeTabLineRef = useRef();
+  const activeTabRef = useRef();
+
+  const activeTabChange = (btn, i) => {
+    const { offsetWidth, offsetLeft } = btn;
+    console.log(offsetWidth,offsetLeft)
+
+    activeTabLineRef.current.style.width = offsetWidth + "px";
+    activeTabLineRef.current.style.left = offsetLeft + "px";
+
+    setInPageNaveIndex(i);
+
+  }
+
+  useEffect(()=> {
+    activeTabChange(activeTabRef.current ,defaultActiveIndex);
+  },[]);
 
   return (
     <>
       <div className='relative mb-8 border-b bg-white border-grey flex flex-nowrap overflow-x-auto'>
         {Routes.map((route, i) => 
           {
-            return <button key={i} className= {'p-4 px-5 capitalize '+ (inPageNaveIndex == i ? "text-black" : "text-grey")}>{route}</button>
+            return <button key={i} 
+                      ref={i == defaultActiveIndex ? activeTabRef : null}
+                      className= {'p-4 px-5 capitalize '+ (inPageNaveIndex == i ? "text-black" : "text-dark-grey")}
+                      onClick={(e) => activeTabChange(e.target, i)}
+                      >{route}</button>
           }
         )}
+
+      <hr ref={activeTabLineRef} className='absolute bottom-0 duration-300' />
+
       </div>
     </>
   );
