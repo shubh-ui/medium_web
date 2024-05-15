b27075434b89a2ae0225b86c087f520c6e6ba3ef
shubham <shubhpatil8510@gmail.com>
2024-05-16 00:14:59 +0530

    api integratin issue fixed and renderd blogpost

--- blogging website - frontend/src/pages/home.page.jsx
+++ blogging website - frontend/src/pages/home.page.jsx
@@ -3,48 +3,59 @@ import InpageNavigation from '../components/inpage-navigation.component'
 import AnimationWrapper from '../common/page-animation'
 import axios from "axios";
 import Loader from '../components/loader.component';
+import BlogPostCard from '../components/blog-post.component';
 
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
 
   const fetchLatestBlogs = () => {
-      const context = "/api";
-      const urlCd = "/latest-blogs";
-      axios.get(context + urlCd).then(({data}) => {
+    const context = "/api";
+    const urlCd = "/latest-blogs";
+    axios
+      .get(context + urlCd)
+      .then(({ data }) => {
         setBlogs(data.blogs);
-      }).catch(err => console.error(err.massage));
-  }
+      })
+      .catch((err) => console.error(err.massage));
+  };
 
   useEffect(()=> {
     fetchLatestBlogs();
   },[])
 
   return (
-      <AnimationWrapper>
-        <section className="flex items-center justify-center gap-10">
-          {/* latest blog.. */}
-          <div className="w-full">
-            <InpageNavigation Routes={["Home", "Trending Blogs"]} defaultHidden={["Trending Blogs"]}>
-              <>
-                {
-                  blogs == null ? <Loader /> :
-                  blogs.map((blog, i) => {
-                    return <AnimationWrapper key={i}>
-                        { blog.title }
+    <AnimationWrapper>
+      <section className="flex items-center justify-center gap-10">
+        {/* latest blog.. */}
+        <div className="w-full">
+          <InpageNavigation
+            Routes={["Home", "Trending Blogs"]}
+            defaultHidden={["Trending Blogs"]}
+          >
+            <>
+              {blogs == null ? (
+                <Loader />
+              ) : (
+                blogs.map((blog, i) => {
+                  return (
+                    <AnimationWrapper
+                      transition={{ duration: 1, delay: i * 0.1 }}
+                      key={i}
+                    >
+                      <BlogPostCard content={blog} author />
                     </AnimationWrapper>
-                  })
-                }
-              </>
+                  );
+                })
+              )}
+            </>
+          </InpageNavigation>
+        </div>
 
-
-            </InpageNavigation>
-          </div>
-
-          {/* trending blogs */}
-          <div></div>
-        </section>
-      </AnimationWrapper>
+        {/* trending blogs */}
+        <div></div>
+      </section>
+    </AnimationWrapper>
   );
 }
 
