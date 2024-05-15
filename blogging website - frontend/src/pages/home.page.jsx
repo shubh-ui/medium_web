80d783785920cc909c6b047f061bfedaad46d36d
shubham <shubhpatil8510@gmail.com>
2024-05-15 18:25:37 +0530

    api integrated for latest blogs and rendered blogs on home screen

--- blogging website - frontend/src/pages/home.page.jsx
+++ blogging website - frontend/src/pages/home.page.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import InpageNavigation from '../components/inpage-navigation.component'
 import AnimationWrapper from '../common/page-animation'
+import axios from "axios";
+import Loader from '../components/loader.component';
+
 
 const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+
+  const fetchLatestBlogs = () => {
+      const context = "/api";
+      const urlCd = "/latest-blogs";
+      axios.get(context + urlCd).then(({data}) => {
+        setBlogs(data.blogs);
+      }).catch(err => console.error(err.massage));
+  }
+
+  useEffect(()=> {
+    fetchLatestBlogs();
+  },[])
+
   return (
       <AnimationWrapper>
         <section className="flex items-center justify-center gap-10">
           {/* latest blog.. */}
           <div className="w-full">
             <InpageNavigation Routes={["Home", "Trending Blogs"]} defaultHidden={["Trending Blogs"]}>
-              <h1>Latest Blogs Here</h1>
-              <h1>Trending Blogs Here</h1>
+              <>
+                {
+                  blogs == null ? <Loader /> :
+                  blogs.map((blog, i) => {
+                    return <AnimationWrapper key={i}>
+                        { blog.title }
+                    </AnimationWrapper>
+                  })
+                }
+              </>
+
+
             </InpageNavigation>
           </div>
 
