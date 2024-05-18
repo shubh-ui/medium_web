cbb9daf212a20be34c79ef0386d0f76c1b3bbc2b
shubham <shubhpatil8510@gmail.com>
2024-05-18 23:39:38 +0530

    GetDate function added for gatting human readable date

---
+++ blogging website - frontend/src/common/date.jsx
@@ -0,0 +1,30 @@
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sup",
+  "Oct",
+  "Nov",
+  "Des",
+];
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+export const getDay = (timestamp) => {
+  let date = new Date(timestamp);
+
+  return `${date.getDate()} ${months[date.getMonth()]}`;
+};
