60553c89cea362ad26667731d32ebab162502b79
shubham <shubhpatil8510@gmail.com>
2024-05-19 01:35:18 +0530

    treding blog sidebar and category tags created

--- blogging website - frontend/src/pages/home.page.jsx
+++ blogging website - frontend/src/pages/home.page.jsx
@@ -10,6 +10,7 @@ import MininamBlogPost from '../components/nobanner-blog-post.component';
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [trendingBlogs, setTrendingBlogs] = useState(null);
+  const categories = ["programing","hollywood","film making","social media", "cooking","tech","finances","travel"];
 
 
   const fetchLatestBlogs = () => {
@@ -42,7 +43,7 @@ const Home = () => {
 
   return (
     <AnimationWrapper>
-      <section className="flex items-center justify-center gap-10">
+      <section className="flex justify-center gap-10">
         {/* latest blog.. */}
         <div className="w-full">
           <InpageNavigation
@@ -59,34 +60,84 @@ const Home = () => {
                       transition={{ duration: 1, delay: i * 0.1 }}
                       key={i}
                     >
-                      <BlogPostCard content={blog} author={blog.author.personal_info} />
+                      <BlogPostCard
+                        content={blog}
+                        author={blog.author.personal_info}
+                      />
                     </AnimationWrapper>
                   );
                 })
               )}
             </>
-          
+
             <>
-              {
-                trendingBlogs == null ? <Loader /> :
+              {trendingBlogs == null ? (
+                <Loader />
+              ) : (
                 trendingBlogs.map((blog, i) => {
                   // {console.log(blog)}
                   return (
                     <AnimationWrapper
-                    transition={{ duration: 1, delay: i * 0.1 }}
-                    key={i}
+                      transition={{ duration: 1, delay: i * 0.1 }}
+                      key={i}
                     >
-                      <MininamBlogPost blog={blog} author={blog.author.personal_info} index={i} />
+                      <MininamBlogPost
+                        blog={blog}
+                        author={blog.author.personal_info}
+                        index={i}
+                      />
                     </AnimationWrapper>
-                  )
+                  );
                 })
-              }
+              )}
             </>
           </InpageNavigation>
         </div>
 
         {/* trending blogs */}
-        <div>
+        <div className="min-w-[40%] lg:min-w-[400px] max-w-min border-l border-grey pl-8 pt-3 max-md:hidden">
+          <div className="flex flex-col gap-10">
+            <div>
+            <h1 className="font-medium text-xl mb-8">
+              Stories from all interests
+            </h1>
+            <div className='flex gap-3 flex-wrap'>
+                {
+                  categories.map((category,i) => {
+                      return <button className='tag' key={i}>
+                          { category }
+                      </button>
+                  })
+                }
+            </div>
+            </div>
+
+          <div>
+            <h1 className="font-medium text-xl mb-8">Trendings <i className="fi fi-rr-arrow-trend-up"></i></h1>
+            
+            <>
+              {trendingBlogs == null ? (
+                <Loader />
+              ) : (
+                trendingBlogs.map((blog, i) => {
+                  // {console.log(blog)}
+                  return (
+                    <AnimationWrapper
+                      transition={{ duration: 1, delay: i * 0.1 }}
+                      key={i}
+                    >
+                      <MininamBlogPost
+                        blog={blog}
+                        author={blog.author.personal_info}
+                        index={i}
+                      />
+                    </AnimationWrapper>
+                  );
+                })
+              )}
+            </>
+          </div>
+          </div>
         </div>
       </section>
     </AnimationWrapper>
