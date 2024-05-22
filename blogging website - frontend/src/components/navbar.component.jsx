f20ac9c15650c3ab17d0d83dbec3019849f948d2
shubham <shubhpatil8510@gmail.com>
2024-05-23 01:02:33 +0530

    search added in navbar

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from "react";
 import logo from "../imgs/logo.png"
-import {Link, Outlet} from "react-router-dom"
+import {Link, Navigate, Outlet, useNavigate} from "react-router-dom"
 import { Toaster } from "react-hot-toast";
 import { userContext } from "../App";
 import UserNavigationPanel from "./user-navigation.component";
@@ -15,6 +15,8 @@ const Navabar = () => {
 
     const {userAuth, userAuth : { access_token, profile_img },} = useContext(userContext);
 
+    const navigate = useNavigate();
+
     const handleUserNavPanel = () => {
       setUsernNavPanel(currentVal => !currentVal);
     }
@@ -23,6 +25,13 @@ const Navabar = () => {
       setTimeout(() => {
       setUsernNavPanel(false);
       }, 300);
+    }
+
+    const handleSearch = (e) => {
+      let query = e.target.value;
+      if(e.keyCode == 13 && query.length) {
+        navigate(`/search/${query}`)
+      }
     }
 
     return (
@@ -38,6 +47,7 @@ const Navabar = () => {
             type="text"
             placeholder="Search"
             className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12"
+            onKeyDown={handleSearch}
           />
           <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-gray"></i>
         </div>
