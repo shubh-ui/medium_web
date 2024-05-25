2545585959f1eb9eeeef82d54c9f6e64cd04b497
shubham <shubhpatil8510@gmail.com>
2024-05-26 01:12:39 +0530

    user card componnet created and users renderd on UI

---
+++ blogging website - frontend/src/components/usercard.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const UserCard = ({ user }) => {
+  console.log(user);
+  let {
+    personal_info: { fullname, username, profile_img },
+  } = user;
+  return (
+    <Link to={`/user/${username}`} className="flex gap-5 items-center mb-5">
+      <img
+        src={profile_img}
+        alt="profile_img"
+        className="h-14 w-14 rounded-full"
+      />
+      <div>
+        <h1 className="font-medium text-xl line-clamp-2">{fullname}</h1>
+        <p className="text-dark-grey">@{username}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default UserCard;
