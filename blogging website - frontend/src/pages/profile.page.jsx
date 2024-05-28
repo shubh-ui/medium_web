b02baad56b60194d97c86755e825483cb781f0fd
shubham <shubhpatil8510@gmail.com>
2024-05-28 23:57:39 +0530

    created profile state and empty profile structure

--- blogging website - frontend/src/pages/profile.page.jsx
+++ blogging website - frontend/src/pages/profile.page.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+
+export const profileDataStructure = {
+  personal_info: {
+    fullname: "",
+    username: "",
+    profile_img: "",
+    bio: "",
+  },
+  account_info: {
+    totla_posts: 0,
+    total_reads: 0,
+  },
+  social_links: {},
+  joinedAt: "",
+};
 
 const ProfilePage = () => {
-    let {id: profileId } = useParams();
-  return (
-    <div>ProfilePage - { profileId }</div>
-  )
-}
+  let { id: profileId } = useParams();
+  const [profile, setProfile] = useState(profileDataStructure);
+  return <div>ProfilePage - {profileId}</div>;
+};
 
-export default ProfilePage
+export default ProfilePage;
