d145db72df77ef62ab64425231f556f9ab04491f
shubham <shubhpatil8510@gmail.com>
2024-05-29 00:17:04 +0530

    loading state added and getprofile api integrated into profile page

--- blogging website - frontend/src/pages/profile.page.jsx
+++ blogging website - frontend/src/pages/profile.page.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import Loader from "../components/loader.component";
 
 export const profileDataStructure = {
   personal_info: {
@@ -19,7 +21,38 @@ export const profileDataStructure = {
 const ProfilePage = () => {
   let { id: profileId } = useParams();
   const [profile, setProfile] = useState(profileDataStructure);
-  return <div>ProfilePage - {profileId}</div>;
+  const [loading, setLoading] = useState(true);
+
+  let {
+    personal_info : { fullname, username, profile_img, bio },
+    account_info: { totla_posts, total_reads },
+    joinedAt,
+  } = profile;
+
+  const fetchUserProfile = () => {
+    const context = "/get-profile";
+    axios
+      .post(import.meta.env.VITE_SERVER_CONTEXT + context, {
+        username: profileId,
+      })
+      .then(({data}) => {
+        console.log(data);
+        setProfile(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchUserProfile();
+  }, []);
+
+  return (
+    <>{loading ? <Loader /> : <div>{fullname}</div>}</>
+  );
 };
 
 export default ProfilePage;
