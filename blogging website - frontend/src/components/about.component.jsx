d6be377e37bdae6b410a42ae26ecc94c1eb812d0
shubham <shubhpatil8510@gmail.com>
2024-05-30 00:37:38 +0530

    About Card social links and user joindon added

--- blogging website - frontend/src/components/about.component.jsx
+++ blogging website - frontend/src/components/about.component.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { getFullDay } from "../common/date";
 
-const AboutUser = ({className, bio, social_links, joinedAt}) => {
+const AboutUser = ({ className, bio, social_links, joinedAt }) => {
   return (
     <div className={"md:w-[90%] md:mt-7 " + className}>
-        <p className='text-xl leading-7'>
-            {bio.length ? bio : "Nothing to read here..."}
-        </p>
-        <div className='flex gap-x-7 gap-y-2 flex-wrap my-7 items-center text-dark-grey'>
-            {
-                Object.keys(social_links).map((key,i) => {
+      <p className="text-xl leading-7">
+        {bio.length ? bio : "Nothing to read here..."}
+      </p>
+      <div className="flex gap-x-7 gap-y-2 flex-wrap my-7 items-center text-dark-grey">
+        {Object.keys(social_links).map((key, i) => {
+          let link = social_links[key];
 
-                    let link = social_links[key];
-                
-                    return link ? <Link key={i} target='_blank' to={link}>
-                        <i className={'fi ' + (key !== 'website' ? "fi-brands-" + key : "fi-rr-globe") + " text-2xl hover:text-black"}></i>
-                    </Link> :""
-                })
-            }
-        </div>
+          return link ? (
+            <Link key={i} target="_blank" to={link}>
+              <i
+                className={
+                  "fi " +
+                  (key !== "website" ? "fi-brands-" + key : "fi-rr-globe") +
+                  " text-2xl hover:text-black"
+                }
+              ></i>
+            </Link>
+          ) : (
+            ""
+          );
+        })}
+      </div>
+      <p className="text-xl leading-7 text-dark-grey">
+        Joined on {getFullDay(joinedAt)}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default AboutUser
+export default AboutUser;
