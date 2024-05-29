5ffd1c8f9d137e026963c81d774d6cb7cc7975f5
shubham <shubhpatil8510@gmail.com>
2024-05-29 10:16:09 +0530

    Reset state and edit user profile button added

--- blogging website - frontend/src/pages/profile.page.jsx
+++ blogging website - frontend/src/pages/profile.page.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import Loader from "../components/loader.component";
+import { userContext } from "../App";
 
 export const profileDataStructure = {
   personal_info: {
@@ -22,10 +23,13 @@ const ProfilePage = () => {
   let { id: profileId } = useParams();
   const [profile, setProfile] = useState(profileDataStructure);
   const [loading, setLoading] = useState(true);
+  let {
+    userAuth: { username },
+  } = useContext(userContext);
 
   let {
-    personal_info : { fullname, username, profile_img, bio },
-    account_info: { totla_posts, total_reads },
+    personal_info: { fullname, username: profileName, profile_img, bio },
+    account_info: { total_posts, total_reads },
     joinedAt,
   } = profile;
 
@@ -35,7 +39,7 @@ const ProfilePage = () => {
       .post(import.meta.env.VITE_SERVER_CONTEXT + context, {
         username: profileId,
       })
-      .then(({data}) => {
+      .then(({ data }) => {
         console.log(data);
         setProfile(data);
         setLoading(false);
@@ -47,11 +51,50 @@ const ProfilePage = () => {
   };
 
   useEffect(() => {
+    resetState();
     fetchUserProfile();
-  }, []);
+  }, [profileId]);
+
+  const resetState = () => {
+    setProfile(profileDataStructure);
+    setLoading(true);
+  };
 
   return (
-    <>{loading ? <Loader /> : <div>{fullname}</div>}</>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <section className="md:flex flex-row-reverse items-start gap-5 min-[1100px]:gap-12">
+          <div className="flex flex-col max-md:items-center gap-5 min-w-[250px]">
+            <img
+              className="w-48 h-48 bg-grey rounded-full md:w-32 md:h-32"
+              src={profile_img}
+              alt="profile_img"
+            />
+            <h1 className="text-2xl font-medium">@{profileName}</h1>
+            <p className="text-xl capitalize h-6">{fullname}</p>
+            <p>
+              {total_posts.toLocaleString()} Blogs -{" "}
+              {total_reads.toLocaleString()} - Reads
+            </p>
+
+            <div className="flex gap-4 mt-2">
+              {username === profileName ? (
+                <Link
+                  to="/settings/edit-profile"
+                  className="btn-light rounded-md"
+                >
+                  Edit Profile
+                </Link>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
