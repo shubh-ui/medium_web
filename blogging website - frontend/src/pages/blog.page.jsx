9f01c02f8c96cd4dd161b1b796d3646e2cd8c20b
shubham <shubhpatil8510@gmail.com>
2024-06-02 00:23:17 +0530

    Get blog api integration done on blog page

--- blogging website - frontend/src/pages/blog.page.jsx
+++ blogging website - frontend/src/pages/blog.page.jsx
@@ -1,8 +1,25 @@
+import axios from "axios";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom"
 
 const BlogPage = () =>  {
 
     let { blogId } = useParams();
+
+    const fetchBlog = () => {
+      const context = "/get-blog";
+      axios
+        .post(import.meta.env.VITE_SERVER_CONTEXT + context, { blogId })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    useEffect(() => {
+        fetchBlog();
+    }, []);
     return (
         <>This is a blog page- {blogId}</>
     )
