dd316612939b4650a3d93d8fd049f71850bf67df
shubham <shubhpatil8510@gmail.com>
2024-06-04 00:41:01 +0530

    basic banner and user details UI created for blog page

--- blogging website - frontend/src/pages/blog.page.jsx
+++ blogging website - frontend/src/pages/blog.page.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
+import AnimationWrapper from "../common/page-animation";
+import Loader from "../components/loader.component";
 
 export const blogStructure = {
   title: "",
@@ -14,7 +16,8 @@ export const blogStructure = {
 
 const BlogPage = () => {
   let { blogId } = useParams();
-  const [blog, setBlog] = useState(blogStructure);
+  const [ blog, setBlog ] = useState(blogStructure);
+  const [ loading, setLoading ] = useState(true);
 
   let {
     title,
@@ -25,7 +28,7 @@ const BlogPage = () => {
     blog_id,
     des,
     author: {
-      personal_info: { username, fullname, profile_img },
+      personal_info: { username:author_username, fullname, profile_img },
     },
   } = blog;
 
@@ -36,15 +39,47 @@ const BlogPage = () => {
       .then(({ data: { blog } }) => {
         console.log(blog);
         setBlog(blog);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
+        setLoading(false);
+
       });
   };
   useEffect(() => {
     fetchBlog();
   }, []);
-  return <>This is a blog page- {blogId}</>;
+  return (
+    <AnimationWrapper>
+      {loading ? (
+        <Loader />
+      ) : (
+       <section>
+         <div className="max-w[900px] center py-10 max-lg:px-[5vw]">
+          <img src={banner} alt="blog_img" className="aspect-video" />
+          <div className="mt-12">
+              <h2>{ title }</h2>  
+              <div className="flex max-sm:flex-col justify-between my-8">
+                <div className="flex gap-5 items-start">
+                    <img src={profile_img} className="w-12 h-12 rounded-full" alt="profiel_img" />
+                    <div className="flex flex-col">
+                    <p className="capitalize">{fullname}</p>
+                    <div>
+                        @
+                    <Link to={`/user/${author_username}`} className="underline">{author_username}</Link>
+
+                    </div>
+                    </div>
+                </div>
+
+              </div>
+          </div>
+        </div>
+       </section>
+      )}
+    </AnimationWrapper>
+  );
 };
 
 export default BlogPage
