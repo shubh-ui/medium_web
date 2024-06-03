d9bbcdf252c338e324b09d421155c3b9d5795a85
shubham <shubhpatil8510@gmail.com>
2024-06-04 01:22:13 +0530

    created a blog context and author details UI

--- blogging website - frontend/src/pages/blog.page.jsx
+++ blogging website - frontend/src/pages/blog.page.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom"
 import AnimationWrapper from "../common/page-animation";
 import Loader from "../components/loader.component";
 import { getDay } from "../common/date";
+import BlogInteraction from "../components/blog-interaction.component";
 
 export const blogStructure = {
   title: "",
@@ -14,6 +15,7 @@ export const blogStructure = {
   des: "",
   author: { personal_info: { username: "", fullname: "", profile_img: "" } },
 };
+export const  blogContext = createContext({})
 
 const BlogPage = () => {
   let { blogId } = useParams();
@@ -56,29 +58,42 @@ const BlogPage = () => {
       {loading ? (
         <Loader />
       ) : (
-       <section>
-         <div className="max-w[900px] center py-10 max-lg:px-[5vw]">
-          <img src={banner} alt="blog_img" className="aspect-video" />
-          <div className="mt-12">
-              <h2>{ title }</h2>  
-              <div className="flex max-sm:flex-col justify-between my-8">
-                <div className="flex gap-5 items-start">
-                    <img src={profile_img} className="w-12 h-12 rounded-full" alt="profiel_img" />
+        <section>
+          <div className="max-w[900px] center py-10 max-lg:px-[5vw]">
+            <img src={banner} alt="blog_img" className="aspect-video" />
+            <blogContext.Provider value={{ blog, setBlog}}>
+              <div className="mt-12">
+                <h2>{title}</h2>
+                <div className="flex max-sm:flex-col justify-between my-8">
+                  <div className="flex gap-5 items-start">
+                    <img
+                      src={profile_img}
+                      className="w-12 h-12 rounded-full"
+                      alt="profiel_img"
+                    />
                     <div className="flex flex-col">
-                    <p className="capitalize">{fullname}</p>
-                    <div>
+                      <p className="capitalize">{fullname}</p>
+                      <div>
                         @
-                    <Link to={`/user/${author_username}`} className="underline">{author_username}</Link>
-
+                        <Link
+                          to={`/user/${author_username}`}
+                          className="underline"
+                        >
+                          {author_username}
+                        </Link>
+                      </div>
                     </div>
-                    </div>
+                  </div>
+                  <p className="text-dark-grey opacity-75 max-sm:mt-6 max-sm:ml-12 max-sm:pl-5">
+                    Published On {getDay(publishedAt)}
+                  </p>
                 </div>
-                <p className="text-dark-grey opacity-75 max-sm:mt-6 max-sm:ml-12 max-sm:pl-5">Published On {getDay(publishedAt)}</p>
-
               </div>
+              <BlogInteraction />
+
+            </blogContext.Provider>
           </div>
-        </div>
-       </section>
+        </section>
       )}
     </AnimationWrapper>
   );
