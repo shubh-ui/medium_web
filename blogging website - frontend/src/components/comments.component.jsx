d8eb5983fedfa915d3619577e228f03dfab13d90
shubham <shubhpatil8510@gmail.com>
2024-06-17 23:47:38 +0530

    Baasic comment compoennt created

---
+++ blogging website - frontend/src/components/comments.component.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+import { blogContext } from '../pages/blog.page';
+
+const CommentsComponent = () => {
+
+    let { commentsWrapper, setCommentsWrapper } = useContext(blogContext);
+  return (
+    <div className="max-sm:w-full fixed duration-700 max-sm:right-0 sm:top-0 w-[30%] min-w-[350px] h-full z-50 bg-white shadow-2xl p-8 px-16 overflow-y-auto overflow-x-hidden">
+      CommentsComponent
+    </div>
+  );
+}
+
+export default CommentsComponent
