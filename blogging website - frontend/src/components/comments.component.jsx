905cc5a2e80c7cac9a9f51a29abb659400953620
shubham <shubhpatil8510@gmail.com>
2024-06-19 23:56:28 +0530

    Comments drawer created with heading

--- blogging website - frontend/src/components/comments.component.jsx
+++ blogging website - frontend/src/components/comments.component.jsx
@@ -3,10 +3,19 @@ import { blogContext } from '../pages/blog.page';
 
 const CommentsComponent = () => {
 
-    let { commentsWrapper, setCommentsWrapper } = useContext(blogContext);
+    let { blog: { title }, commentsWrapper, setCommentsWrapper } = useContext(blogContext);
   return (
     <div className={"max-sm:w-full fixed " + ( commentsWrapper ? "top-0 sm:right-0" : "top-[100%] sm:right-[-100%]") + " duration-700 max-sm:right-0 sm:top-0 w-[30%] min-w-[350px] h-full z-50 bg-white shadow-2xl p-8 px-16 overflow-y-auto overflow-x-hidden"}>
-      CommentsComponent
+      <div className='relative' >
+        <h1 className='text-xl font-medium'>Comments</h1>
+        <p className='text-lg mt-2w-[70%] text-dark-grey line-clamp-1'>{ title }</p>
+
+        <button className='absolute top-0 right-0 flex justify-center items-center w-12 h-12 rounded-full bg-grey'
+          onClick={() => setCommentsWrapper(preVal => !preVal)}
+        >
+          <i className='fi fi-br-cross text-2xl mt-1'></i>
+        </button>
+      </div>
     </div>
   );
 }
