620df3554ca8eadaeb8e124776d641468d793f43
shubham <shubhpatil8510@gmail.com>
2024-06-25 23:46:49 +0530

    log out comment handled

--- blogging website - frontend/src/components/comment-field.component.jsx
+++ blogging website - frontend/src/components/comment-field.component.jsx
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { userContext } from "../App";
+import toast, { Toaster } from "react-hot-toast";
 
 const CommentField = ({action}) => {
   const [comment, setComment] = useState("");
+
+  let { userAuth: { access_token } } = useContext(userContext);
+
+  const handleComment = () => {
+    if(!access_token) {
+      return toast.error("Login is required to leave a comment");
+    }
+  }
   return (
     <>
+      <Toaster />
       <textarea value={comment} placeholder="Leave a comment..."
         className="input-box pl-5 placeholder:text-dark-grey resize-none h-[150px] overflow-auto"
         onChange={(e) => setComment(e.target.value)}
       ></textarea>
 
-      <button className="btn-dark mt-5 px-10">{action}</button>
+      <button className="btn-dark mt-5 px-10"
+        onClick={handleComment}
+      >{action}</button>
     </>
   );
 };
